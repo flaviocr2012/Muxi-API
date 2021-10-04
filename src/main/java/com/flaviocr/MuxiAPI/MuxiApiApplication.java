b3071e1511c7425cc0f4a645e8847e4ed55f02ac
flavio <flaviocr2012@gmail.com>
2021-10-04 16:19:52 -0300

    Initial configs (database, pom)

---
+++ src/main/java/com/flaviocr/MuxiAPI/MuxiApiApplication.java
@@ -0,0 +1,13 @@
+package com.flaviocr.MuxiAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuxiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuxiApiApplication.class, args);
+	}
+
+}
