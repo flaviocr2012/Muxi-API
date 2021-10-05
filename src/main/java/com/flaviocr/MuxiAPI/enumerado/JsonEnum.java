ecd77ef6cf69efc72ffdbd739e80e454b195224f
flavio <flaviocr2012@gmail.com>
2021-10-04 21:54:35 -0300

    Adição do Swagger e Web config.

---
+++ src/main/java/com/flaviocr/MuxiAPI/enumerado/JsonEnum.java
@@ -0,0 +1,61 @@
+package com.flaviocr.MuxiAPI.enumerado;
+
+import java.security.KeyStore;
+
+public enum JsonEnum {
+
+    SCHEMA(
+            "{\r\n" +
+
+                    "	\"title\": \"Terminal\",\r\n" +
+                    "	\"type\": \"object\",\r\n" +
+                    "	\"properties\": {\r\n" +
+                    "		\"logic\": {\r\n" +
+                    "			\"type\": \"integer\"\r\n" +
+                    "		},\r\n" +
+                    "		\"serial\": {\r\n" +
+                    "			\"type\": \"string\"\r\n" +
+                    "		},\r\n" +
+                    "		\"sam\": {\r\n" +
+                    "			\"type\": \"integer\",\r\n" +
+                    "			\"minimum\": 0\r\n" +
+                    "		},\r\n" +
+                    "		\"ptid\": {\r\n" +
+                    "			\"type\": \"string\"\r\n" +
+                    "		},\r\n" +
+                    "		\"plat\": {\r\n" +
+                    "			\"type\": \"integer\"\r\n" +
+                    "		},\r\n" +
+                    "		\"version\": {\r\n" +
+                    "			\"type\": \"string\"\r\n" +
+                    "		},\r\n" +
+                    "		\"mxr\": {\r\n" +
+                    "			\"type\": \"integer\"\r\n" +
+                    "		},\r\n" +
+                    "		\"VERFM\": {\r\n" +
+                    "			\"type\": \"string\"\r\n" +
+                    "		}\r\n" +
+                    "	},\r\n" +
+                    "	\"required\": [\r\n" +
+                    "		\"logic\",\r\n" +
+                    "		\"serial\",\r\n" +
+                    "		\"model\",\r\n" +
+                    "		\"version\"\r\n" +
+                    "	]\r\n" +
+                    "}\r\n" +
+                    "");
+
+    private String schema;
+
+    JsonEnum(String schema) {
+        this.schema = schema;
+    }
+
+    public String getSchema() {
+        return schema;
+    }
+
+    public void setSchema(String schema) {
+        this.schema = schema;
+    }
+}
